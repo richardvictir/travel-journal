b295d14b250d640295f13daa84211fad43144724
Richard Umaru <93866646+richardvictir@users.noreply.github.com>
2022-10-31 17:38:47 +0100

    Add files via upload

---
+++ About.js
@@ -0,0 +1,30 @@
+import React,{Component } from "react";
+
+class About extends React.Component
+{
+  render(){
+    return(
+    <div className="golden">
+    <h3>ABOUT</h3>
+    <p>
+    we deal in all sort of clothes like jumpsuits,long sleeve,short sleeves,gown.We also deal in versace for both male and female,and also vintage
+    
+    
+    
+    </p>
+    
+    
+    </div>
+      )
+    
+  }
+  
+  
+  
+};
+
+
+export default About;
+
+
+
